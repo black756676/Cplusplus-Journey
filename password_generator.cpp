9d9fd5b648b0267dd6d33636cfd0d5e31a77ad6b
Amit-codes <panwaramiy74@gmail.com>
2026-05-29 18:25:28 +0530

    Add password generator implementation in C++
    This file implements a simple password generator in C++ that allows users to specify the length of the password and includes various character types.

---
+++ password_generator.cpp
@@ -0,0 +1,22 @@
+## Password Generator
+
+A simple C++ password generator that creates a random password based on user-defined length.
+
+### Features
+- User enters password length
+- Generates random password
+- Uses uppercase letters
+- Uses lowercase letters
+- Uses numbers
+- Uses special characters
+- Validates minimum password length
+
+### Concepts Used
+- string
+- rand()
+- srand()
+- time(0)
+- for loop
+- if condition
+- string.length()
+- character indexing
