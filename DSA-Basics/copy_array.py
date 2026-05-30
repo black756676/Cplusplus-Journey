335fdbec1e2ceffc493359c982f454ff407a591c
Amit-codes <panwaramiy74@gmail.com>
2026-05-30 20:59:39 +0530

    Add array copying functionality in copy_array.py
    1. Array/List
       arr stores multiple values.
    
    2. Empty List
       copy_arr = [] creates an empty list.
    
    3. for loop
       Loop visits each element one by one.
    
    4. len(arr)
       Gives total number of elements in arr.
    
    5. index i
       i tells the current position.
    
    6. arr[i]
       Gives the value present at index i.
    
    7. append()
       Adds a value at the end of a list.

---
+++ DSA-Basics/copy_array.py
@@ -0,0 +1,12 @@
+# Copy elements from one array to another array//Original Array: [10, 20, 30]
+Copied Array: [10, 20, 30]
+
+arr = [10, 20, 30]
+
+copy_arr = []
+
+for i in range(len(arr)):
+    copy_arr.append(arr[i])
+
+print("Original Array:", arr)
+print("Copied Array:", copy_arr)
