fd4bdf0b526bca3fe097ffb188b64683af8b41b2
Amit-codes <panwaramiy74@gmail.com>
2026-06-01 13:33:44 +0530

    Remove addd and subtract_specific_element.py
    Removed the addd and subtract_specific_element.py file, which contained examples for adding and re1. Array/List
       arr stores multiple values.
    
    2. remove()
       remove(value) removes the first matching value from the list.
    
    3. Specific Element
       In this program, 30 is the element we want to remove.
    
    4. print()
       Used to display the updated array/list.
    moving elements from a list.

---
+++ DSA-Basics/remove addd and subtract_specific_element.py
@@ -0,0 +1,24 @@
+#Start: to add specific element 
+arr = [10, 20, 30]
+
+arr.append(40)
+
+Now:
+arr = [10, 20, 30, 40]
+#second ---- to remove last element 
+arr = [10, 20, 30, 40]
+
+arr.pop()
+
+40 remove ho gaya
+
+Now:
+arr = [10, 20, 30]
+#third-----
+# Remove a specific element from an array/list
+
+arr = [10, 20, 30, 40]
+
+arr.remove(30)
+
+print("Array after removing element:", arr)
